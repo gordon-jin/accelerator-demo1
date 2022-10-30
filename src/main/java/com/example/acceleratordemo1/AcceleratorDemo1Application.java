9fd34399152150f0cca30f217d621ee09a1163a3
gordon-jin <jinchengguo1028@hotmail.com>
2022-10-30 11:10:54 +0900

    Initial commit

---
+++ src/main/java/com/example/acceleratordemo1/AcceleratorDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.acceleratordemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceleratorDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcceleratorDemo1Application.class, args);
+    }
+
+}
